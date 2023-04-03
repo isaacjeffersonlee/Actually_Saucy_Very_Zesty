fc34d4ed17b2cecd547e20643ecb9bec1254b2c2
isaacjeffersonlee <27ilee01@gmail.com>
2023-04-04 01:04:33 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# (A)ctually (S)aucy (V)ery (Z)esty Bot
+
+## Installation
+1. Install the repo:
+```
+git clone https://github.com/isaacjeffersonlee/Actually_Saucy_Very_Zesty
+```
+2. Enter the repo directory and install the required dependencies. (I would recommend doing
+this in a separate python virtual environment).
+```
+cd Actually_Saucy_Very_Zesty/
+python -m venv asvz_env
+source asvz_env/bin/activate
+pip install -r requirements.txt
+```
+
+Then everything should be installed.
+
+## Config File
+We specify our user id and password and what sports we would like to enroll for
+in the config.yaml file.
+This looks like:
+```yaml
+asvz_id: "changeme"
+asvz_password: "changeme"
+datetime_format: "%d.%m.%Y %H:%M"
+urls: # Replace these with your own urls
+  - "https://schalter.asvz.ch/tn/lessons/493925"
+  - "https://schalter.asvz.ch/tn/lessons/440705"
+  - "https://schalter.asvz.ch/tn/lessons/434726"
+
+enrollment_times: # Replace these with the online enrollment times of the above urls.
+  - "03.04.2023 11:05"
+  - "03.04.2023 11:15"
+  - "04.04.2023 07:45"
+```
+Firstly change the `asvz_id` and `asvz_password` fields to your corresponding
+id and password. (id can be found in your settings/profile on the asvz website).
+
+For each lesson we want to enroll for, go to https://asvz.ch/426-sportfahrplan
+and find its corresponding url. E.g https://schalter.asvz.ch/tn/lessons/493925
+(Note: in case you haven't used the ASVZ website before,
+each lesson has its own unique webpage for each time.)
+
+Then at the bottom of the page find the section that looks like:
+```
+Einschreibezeitraum
+Mo, 03.04.2023 10:00 - Di, 04.04.2023 06:00
+```
+And copy the corresponding online enrollment start time to the list of enrollment_times.
+So for this example, this would be: `"03.04.2023 10:00"`.
+Make sure these are in the format specified by time_format.
+Also make sure for every url above, there is a matching enrollment time below.
+The bot will throw an error if the number of urls does not match the number
+of enrollment times.
+
+## Running the bot
+Once everything has been installed correctly and the config has been set,
+you should be able to run the bot with:
+```shell
+python bot.py
+```
+It will open a browser window and attempt to login and then wait to enroll.
+Enrollments are sorted by their enrollment times, and we enroll in a loop
+and then sleep when far away from the next enrollment. If the browser window is closed
+but the loop is still running, the bot will attempt to open the window again when
+it needs to enroll. So it *should be* safe to close the window and leave the script running
+when enrollment times are far apart, although I would advise against it.
+
+I would advice using this script close to the enrollment time, to avoid errors.
+
+## Errors
+If you encounter any errors, please contact me and I'll do my best to fix them ASAP,
+or alternatively raise a GitHub issue in this repo.
---
+++ bot.py
@@ -0,0 +1,163 @@
+import yaml
+import time
+import datetime
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import (
+    TimeoutException,
+    StaleElementReferenceException,
+    NoSuchWindowException,
+)
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.remote.webelement import WebElement
+from rich.console import Console  # Pretty printing
+
+console = Console()
+
+
+def waited_find_element(driver, by: By, value: str, timeout: int = 2) -> WebElement:
+    return WebDriverWait(driver, timeout).until(
+        EC.presence_of_element_located((by, value))
+    )
+
+
+def login(driver: WebDriver, asvz_id: str, asvz_password: str) -> None:
+    try:
+        waited_find_element(driver=driver, by=By.ID, value="AsvzId").send_keys(asvz_id)
+        waited_find_element(driver=driver, by=By.ID, value="Password").send_keys(
+            asvz_password
+        )
+        waited_find_element(
+            driver=driver,
+            by=By.XPATH,
+            value='//*[@id="collapse_asvz"]/div/form/div[3]/button',
+        ).click()
+        console.log("Success: Logged in!")
+    except (TimeoutException, StaleElementReferenceException) as e:
+        console.log("Error: Logging in!")
+        pass
+
+
+def click_login_button(driver: WebDriver) -> None:
+    console.log("Looking for login button...")
+    try:
+        # Get the button to go to the login page
+        waited_find_element(
+            driver=driver,
+            by=By.XPATH,
+            value="/html/body/app-root/div/div[2]/app-lesson-details/"
+            + "div/div/app-lessons-enrollment-button/button",
+        ).click()
+        console.log("Login button clicked!")
+    except TimeoutException:
+        console.log("Unable to find the login button before timeout!")
+
+
+def click_enroll_button(driver: WebDriver) -> None:
+    console.log("Clicking enroll button...")
+    # ASVZ Likes to change these, probably to throw people like us off.
+    possible_xpaths = (
+        '//*[@id="btnRegister"]',
+        '//*[@id="btnenroll"]',
+        '//*[@id="btnEnroll"]',
+        '//*[@id="btnregister"]',
+    )
+    for xpath in possible_xpaths:
+        try:
+            waited_find_element(driver=driver, by=By.XPATH, value=xpath).click()
+            console.log("enroll button clicked!")
+            return None
+        except TimeoutException:
+            continue
+
+    console.log("ERROR: Unable to find the enroll button before timeout!")
+    console.log("This is probably due to ASVZ changing their webpage layout.")
+    console.log("Get in touch if this happens and I'll fix it.")
+
+
+def enroll(
+    driver,
+    oe_time: datetime.datetime,
+    url: str,
+    asvz_id: str,
+    asvz_password: str,
+) -> None:
+    while datetime.datetime.now() < oe_time - datetime.timedelta(seconds=30):
+        time.sleep(2)
+        console.log(
+            f"Waiting to enroll for: {url} @ {oe_time} | {datetime.datetime.now()}"
+        )
+
+    print(f"Broken out of wait loop, time is: {datetime.datetime.now()}")
+    print("Loading webpage...")
+    driver.get(url)
+    print("Clicking login buttons...")
+    click_login_button(driver)
+    login(driver, asvz_id=asvz_id, asvz_password=asvz_password)
+    while True:
+        time.sleep(0.01)
+        if datetime.datetime.now() >= oe_time:
+            print("Clicking enroll button...")
+            click_enroll_button(driver)
+            time.sleep(100)
+            break
+    print("Finished!")
+
+
+def main():
+    with open("config.yaml", "r") as f:
+        config = yaml.safe_load(f)
+
+    asvz_id = str(config["asvz_id"])
+    asvz_password = str(config["asvz_password"])
+    datetime_format = config["datetime_format"]
+    urls = config["urls"]
+    oe_times = config["enrollment_times"]  # online enrollment times
+    assert len(urls) == len(oe_times), (
+        "The number of URLs and enrollment times must be the same!"
+        + f" But {len(urls)} urls found and {len(oe_times)} enrollment times found!"
+    )
+    # Convert to datetime objects
+    oe_times = [
+        datetime.datetime.strptime(oe_time, datetime_format) for oe_time in oe_times
+    ]
+    url_oe_time_pairs = list(zip(urls, oe_times))
+    url_oe_time_pairs.sort(key=lambda pair: pair[1])  # Sort by date
+    console.log("Config loaded!")
+    console.log("Check the following is correct:")
+    for url, oe_time in url_oe_time_pairs:
+        console.log(f"Enrollment time: {oe_time} | URL: {url}")
+
+    input("Press any key to continue...")
+    while True:
+        try:
+            chrome_options = Options()
+            # chrome_options.add_argument("--headless")
+            driver = webdriver.Chrome(options=chrome_options)
+            for url, oe_time in url_oe_time_pairs:
+                driver.get(url)
+                enroll(
+                    driver=driver,
+                    oe_time=oe_time,
+                    url=url,
+                    asvz_id=asvz_id,
+                    asvz_password=asvz_password,
+                )
+
+            time.sleep(100)
+            driver.quit()
+            console.log("Finished enrolling!")
+            break
+        except NoSuchWindowException:  # If we prematurely close the browser window
+            console.log(
+                "WARNING: Browser window was closed before the bot finished enrolling!"
+            )
+            console.log("Retrying...")
+            time.sleep(2)
+
+
+if __name__ == "__main__":
+    main()
